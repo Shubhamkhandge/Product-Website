ac9e01cbf87f6e6717a3eac2912f539ed8c112ab
ShubhamKhandge <shubhamkhandge2820@gmail.com>
2024-05-30 18:37:36 +0530

    All files uploaded

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+document.getElementById("sidebarToggle").onclick = function () {
+    document.getElementById("mySidenav").style.width = "250px";
+};
+
+document.getElementById("closeBtn").onclick = function () {
+    document.getElementById("mySidenav").style.width = "0";
+};
